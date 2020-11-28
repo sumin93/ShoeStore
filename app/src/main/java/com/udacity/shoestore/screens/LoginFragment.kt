c1095c21fd64c271791ba9857de69cbbd4757ed1
a.sumin <a.sumin@cardsmobile.ru>
2020-11-28 15:17:11 +0300

    Добавил databinding

--- app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+        return binding.root
     }
 }
