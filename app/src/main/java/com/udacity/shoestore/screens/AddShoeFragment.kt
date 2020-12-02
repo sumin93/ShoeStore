c404f51660e3b2fba5b25da34806d37be745bdb5
a.sumin <a.sumin@cardsmobile.ru>
2020-12-02 23:50:24 +0300

    AddShoeFragment.kt

---
+++ app/src/main/java/com/udacity/shoestore/screens/AddShoeFragment.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+
+class AddShoeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_shoe, container, false)
+    }
+}
