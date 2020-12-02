b4fc1e5d0a68fc5d3f671b6ccf3fa28719af392b
a.sumin <a.sumin@cardsmobile.ru>
2020-12-02 19:49:51 +0300

    Launch TutorialFragment.kt

---
+++ app/src/main/java/com/udacity/shoestore/screens/TutorialFragment.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentTutorialBinding
+
+class TutorialFragment : Fragment() {
+
+    private lateinit var binding: FragmentTutorialBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_tutorial,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
--- app/src/main/java/com/udacity/shoestore/screens/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/WelcomeFragment.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_welcome,
+                container,
+                false
+        )
+        binding.buttonGoToTutorial.setOnClickListener(Navigation.createNavigateOnClickListener(
+                WelcomeFragmentDirections.actionWelcomeFragmentToTutorialFragment()
+        ))
+        return binding.root
     }
 }
