95805e1cf6fcc00410da9527e5fd8f3ea6e8d5d8
AlJaKot <internetoe@mail.ru>
2022-08-29 15:05:50 +0300

    remove "size" field

--- app/src/main/java/com/udacity/shoestore/models/Shoe.kt
+++ app/src/main/java/com/udacity/shoestore/models/Shoe.kt
@@ -6,7 +6,6 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Shoe(
     var name: String,
-    var size: Double,
     var company: String,
     var description: String
 ) : Parcelable
